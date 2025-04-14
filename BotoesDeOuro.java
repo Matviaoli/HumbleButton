3a6eff8baf94ce69c01390939de81ff047eea508
Matviaoli <96087731+Matviaoli@users.noreply.github.com>
2025-04-14 13:55:51 -0400

    Add files via upload

---
+++ BotoesDeOuro.java
@@ -0,0 +1,14 @@
+package botoesdeouro;
+
+import javax.swing.JFrame;
+
+public class BotoesDeOuro {
+
+    public static void main(String[] args) {
+        Mongolia magnolia = new Mongolia();
+        magnolia.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        magnolia.setSize(550,220);
+        magnolia.setVisible(true);
+    }
+    
+}
---
+++ Mongolia.java
@@ -0,0 +1,46 @@
+package botoesdeouro;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Mongolia extends JFrame{
+    
+    private JButton amendoim, castanha;
+    
+    public Mongolia(){
+        
+        super("oleaginosas");
+        setLayout(new FlowLayout());
+        
+        amendoim = new JButton("botão amendoim");
+        add(amendoim);
+        
+        Icon minduim0 = new ImageIcon(getClass().getResource("mindoim0.jpg"));
+        Icon minduim1 = new ImageIcon(getClass().getResource("mindoim1.jpg"));
+        castanha = new JButton("Botão castanha", minduim0);
+        castanha.setRolloverIcon(minduim1);
+        add(castanha);
+        
+        HumbleButton pistache = new HumbleButton();
+        castanha.addActionListener(pistache);
+        amendoim.addActionListener(pistache);
+        
+    }
+    
+    private class HumbleButton implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            JOptionPane.showMessageDialog(Mongolia.this, String.format("Você precionou: %s", event.getActionCommand()));
+        }
+        
+    }
+    
+    
+}
